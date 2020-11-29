13e90497195f1cb38fbcc7367d0176ba3c292de7
大城由晃 <yoshiaki19980217@icloud.com>
2020-11-29 19:50:09 +0900

    文字削除

--- laraveltest/resources/views/mypage/index.blade.php
+++ laraveltest/resources/views/mypage/index.blade.php
@@ -5,16 +5,15 @@
 <h1>こんにちは{{$name}}です</h1>
 <a href="/mypage/add">投稿する</a>
 <table border=1>
-    <tr>
-        <th>userid</th>
-        <th>コメント</th>
-    </tr>
-    @foreach($items as $item){
-        <tr>
-            <td>{{$item->name}}</td>
-            <td>{{$item->comment}}</td>
-        </tr>
-    @endforeach
-    }
+  <tr>
+    <th>userid</th>
+    <th>コメント</th>
+  </tr>
+  @foreach($items as $item)
+  <tr>
+    <td>{{$item->name}}</td>
+    <td>{{$item->comment}}</td>
+  </tr>
+  @endforeach
 </table>
 @endsection
