b70c1b93a4def15583c61396139aaa4b2f5f4ad4
関海知瑠 <sekimichiru@sekikaichinoMacBook-Air.local>
2020-11-29 20:44:12 +0900

    バリデーション

--- laraveltest/resources/views/mypage/add.blade.php
+++ laraveltest/resources/views/mypage/add.blade.php
@@ -2,18 +2,23 @@
 
 @section('index')
     <a href="/mypage">戻る</a>
-    <form action="/mypage/add" method="post">
-        @csrf
-        @if ($errors->has('comment'))
-        <tr>
-            <th>エラー：</th>
-            <td>{{$errors->first('comment')}}</td>
-        </tr>
-        @endif
-        <tr>
-            <th>comment</th>
-            <td><input type="text" name="comment" value="{{old('comment')}}"></td>
-        </tr>
-        <input type="submit" value="send">
-    </form>
+    <table>
+        <form action="/mypage/add" method="post">
+            @csrf
+            @if ($errors->has('comment'))
+            <tr>
+                <th>エラー：</th>
+                <td>{{$errors->first('comment')}}</td>
+            </tr>
+            @endif
+            <tr>
+                <th>comment</th>
+                <td><input type="text" name="comment" value="{{old('comment')}}"></td>
+            </tr>
+            <tr>
+                <th></th>
+                <td><input type="submit" value="send"></td>
+            </tr>
+        </form>
+    </table>
 @endsection
